fc9547d056490874443829f57ccc655089ec665e
Ashish Jha <ashishjhaofficial@outlook.com>
2018-07-13 18:51:36 +0530

    updated rest.js + css

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,37 +4,6 @@ var Map;
 /**
  * Initialize map as soon as the page is loaded.
  */
-// document.addEventListener('DOMContentLoaded', (event) => {  
-//   initMap();
-// });
-
-/**
- * Initialize leaflet map
- */
-// initMap = () => {
-//   fetchRestaurantFromURL((error, restaurant) => {
-//     if (error) { // Got an error!
-//       console.error(error);
-//     } else {      
-//       self.newMap = L.map('map', {
-//         center: [restaurant.latlng.lat, restaurant.latlng.lng],
-//         zoom: 16,
-//         scrollWheelZoom: false
-//       });
-//       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-//         mapboxToken: '<your MAPBOX API KEY HERE>',
-//         maxZoom: 18,
-//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//         id: 'mapbox.streets'    
-//       }).addTo(newMap);
-//       fillBreadcrumb();
-//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
-//     }
-//   });
-// }  
- 
 window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
@@ -47,9 +16,6 @@ window.initMap = () => {
       });
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-      google.maps.event.addDomListener(window, 'resize', function () {
-        map.setCenter(restaurant.latlng);
-      });
     }
   });
 } 
@@ -92,6 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' Restaurant';
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -129,7 +96,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
